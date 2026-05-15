6b7993d7c49a719a6843632ac39e69bb75aa0ba5
fvukelic <franck.vukelic@gmail.com>
2026-05-15 19:43:34 +0700

    Clean repo

--- constants/images.ts
+++
@@ -1,9 +0,0 @@
-export const images = {
-  mascotLogo: require('../assets/images/moscot-logo.png'),
-  mascotWelcome: require('../assets/images/mascot-welcome.png'),
-  mascotAuth: require('../assets/images/mascot-auth.png'),
-  streakFire: require('../assets/images/streak-fire.png'),
-  earth: require('../assets/images/earth.png'),
-  treasure: require('../assets/images/treasure.png'),
-  palace: require('../assets/images/palace.png'),
-};
